a1108ce03fab7f591fd745b313779f03fb304ccf
Jackhalabardnik <wojcikj2000@protonmail.com>
2022-11-18 19:53:07 +0100

    test - refactor of sensor test

--- test/test_sensor.cpp
+++ test/test_sensor.cpp
@@ -1,7 +1,7 @@
-#include "utils.h"
-#include "testutils.h"
 #include "analogmockinput.h"
 #include "sensor.h"
+#include "testutils.h"
+#include "utils.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -11,70 +11,71 @@ using ::testing::InSequence;
 using ::testing::Return;
 
 TEST(sensor_tests, sensor_inits_analog_device) {
-    auto mock_analog_input = std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
-    EXPECT_CALL(*mock_analog_input, init())                  
-      .Times(AtLeast(1));
+  auto mock_analog_input =
+      std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
+  EXPECT_CALL(*mock_analog_input, init()).Times(AtLeast(1));
 
-    Sensor sensor(std::move(mock_analog_input));
-    sensor.init();
+  Sensor sensor(std::move(mock_analog_input));
+  sensor.init();
 }
 
 TEST(sensor_tests, sensor_gathers_data_from_analog) {
-    constexpr int sensor_readings_num = SensorUtils::WINDOW_SIZE;
-    distribution value_distribution(SensorUtils::MIN_SENSOR_VALUE, SensorUtils::MAX_SENSOR_VALUE);
-    std::vector<int> sensor_values;
+  constexpr int sensor_readings_num = SensorUtils::WINDOW_SIZE;
+  distribution value_distribution(SensorUtils::MIN_SENSOR_VALUE,
+                                  SensorUtils::MAX_SENSOR_VALUE);
+  std::vector<int> sensor_values;
 
-    for(int i = 0; i < sensor_readings_num; i++) {
-        sensor_values.push_back(value_distribution(rng));
-    }
+  for (int i = 0; i < sensor_readings_num; i++) {
+    sensor_values.push_back(value_distribution(rng));
+  }
 
-    auto mock_analog_input = std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
+  auto mock_analog_input =
+      std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
 
-    InSequence s;
-    for (int i = 0; i <= sensor_readings_num; i++) {
-        EXPECT_CALL(*mock_analog_input, get_value())
-            .WillOnce(Return(sensor_values[i]))
-            .RetiresOnSaturation();
-    }
-      
-    Sensor sensor(std::move(mock_analog_input));
+  InSequence s;
+  for (int i = 0; i <= sensor_readings_num; i++) {
+    EXPECT_CALL(*mock_analog_input, get_value())
+        .WillOnce(Return(sensor_values[i]))
+        .RetiresOnSaturation();
+  }
 
-    for (int i = 0; i <= sensor_readings_num; i++) {
-        sensor.measure_brightness();
-        double expected_max_brightness = *std::max_element(sensor_values.begin(), sensor_values.begin() + i+1);
-        EXPECT_DOUBLE_EQ(sensor.get_denoised_value(), expected_max_brightness) << "Problem with iteration: " << i;
-    }
+  Sensor sensor(std::move(mock_analog_input));
+
+  for (int i = 0; i <= sensor_readings_num; i++) {
+    sensor.measure_brightness();
+    double expected_max_brightness =
+        *std::max_element(sensor_values.begin(), sensor_values.begin() + i + 1);
+    EXPECT_DOUBLE_EQ(sensor.get_denoised_value(), expected_max_brightness)
+        << "Problem with iteration: " << i;
+  }
 }
 
 TEST(sensor_tests, sensor_keeps_only_readings_in_a_window) {
-    constexpr int sensor_readings_to_generate = SensorUtils::WINDOW_SIZE-1,
-                    sensor_readings_to_loop = SensorUtils::WINDOW_SIZE+1;
-    distribution value_distribution(SensorUtils::MIN_SENSOR_VALUE,SensorUtils::MAX_SENSOR_VALUE-2);
-    std::vector<int> sensor_values;
+  constexpr int sensor_readings_to_generate = SensorUtils::WINDOW_SIZE - 1,
+                sensor_readings_to_loop = SensorUtils::WINDOW_SIZE + 1;
 
-    for(int i = 0; i < sensor_readings_to_generate; i++) {
-        sensor_values.push_back(value_distribution(rng));
-    }
+  auto mock_analog_input =
+      std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
 
-    auto mock_analog_input = std::unique_ptr<AnalogMockInput>(new AnalogMockInput());
+  InSequence s;
+  EXPECT_CALL(*mock_analog_input, get_value())
+      .WillOnce(Return(SensorUtils::MAX_SENSOR_VALUE))
+      .RetiresOnSaturation();
 
-    InSequence s;
+  for (int i = 0; i <= sensor_readings_to_generate; i++) {
     EXPECT_CALL(*mock_analog_input, get_value())
-            .WillOnce(Return(SensorUtils::MAX_SENSOR_VALUE))
-            .RetiresOnSaturation();
-    for (int i = 0; i <= sensor_readings_to_generate; i++) {
-        EXPECT_CALL(*mock_analog_input, get_value())
-            .WillOnce(Return(sensor_values[i]))
-            .RetiresOnSaturation();
-    }
-    EXPECT_CALL(*mock_analog_input, get_value())
-            .WillOnce(Return(SensorUtils::MAX_SENSOR_VALUE-1))
-            .RetiresOnSaturation();
-      
-    Sensor sensor(std::move(mock_analog_input));
+        .WillOnce(Return(SensorUtils::MIN_SENSOR_VALUE))
+        .RetiresOnSaturation();
+  }
 
-    for (int i = 0; i <= sensor_readings_to_loop; i++) {
-        sensor.measure_brightness();
-    }
-    EXPECT_EQ(sensor.get_denoised_value(), SensorUtils::MAX_SENSOR_VALUE-1);
+  EXPECT_CALL(*mock_analog_input, get_value())
+      .WillOnce(Return(SensorUtils::MAX_SENSOR_VALUE - 1))
+      .RetiresOnSaturation();
+
+  Sensor sensor(std::move(mock_analog_input));
+
+  for (int i = 0; i <= sensor_readings_to_loop; i++) {
+    sensor.measure_brightness();
+  }
+  EXPECT_EQ(sensor.get_denoised_value(), SensorUtils::MAX_SENSOR_VALUE - 1);
 }
